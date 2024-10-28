4b11ef09b234450876d29698076da235d2f440e3
Dares34 <GorodiskiyDen@gmail.com>
2024-10-28 22:59:29 +0300

    add room create and new test for him

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from user.models import User
+from room.models import Room, Participant
+from quiz.models import Question, Quiz
 
 
 class AuthTestCase(APITestCase):
@@ -23,3 +25,28 @@ class AuthTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data, {'error': 'Invalid credentials'})
+
+
+class RoomViewSetTests(APITestCase):
+    def test_create_room(self):
+        data = {
+            'quizSubject': 'История',
+            'timer': 30
+        }
+        url = reverse('room-list')
+        response = self.client.post(url, data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Room.objects.count(), 1)
+        self.assertEqual(Room.objects.get().quizSubject, 'История')
+    
+    def test_create_room_invalid_data(self):
+        data = {
+            'quizSubject': '',
+            'timer': -10
+        }
+        url = reverse('room-list')
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Room.objects.count(), 0)
+
--- room/models.py
+++ room/models.py
@@ -3,17 +3,17 @@ from user.models import User
 
 
 class Room(models.Model):
-    id = models.IntegerField(primary_key=True, serialize=True)
+    # id = models.IntegerField(primary_key=True, serialize=True)
     # name = models.CharField(max_length=255)
     quizSubject = models.CharField(max_length=255)
     timer = models.IntegerField()
 
-    def __str__(self):
-        return self.id
+    # def __str__(self):
+    #     return self.
 
 
 class Participant(models.Model):
-    id = models.IntegerField(primary_key=True, serialize=True)
+    # id = models.IntegerField(primary_key=True, serialize=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     roomId = models.ForeignKey(Room, on_delete=models.CASCADE)
     score = models.IntegerField()
