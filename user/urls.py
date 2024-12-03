a440c1f87207537a74af56d1802434197e124f32
1K4S1-L1MB0 <Egor.gry2003@mail.ru>
2024-12-03 10:24:07 +0300

    micro fix urls

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-
 # schema_view = get_schema_view(
 #     openapi.Info(
 #         title="User API",
